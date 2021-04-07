21130d4b859d04f15556f91f1bdea17aff3b5b0c
william50307 <william5030788@gmail.com>
2021-04-08 05:10:38 +0800

    add api_root, chnage LED viewsets, add gitignore file to ignore pycache

--- LED/serializers.py
+++ LED/serializers.py
@@ -3,17 +3,18 @@ from rest_framework import serializers
 from LED.models import LED
 
 
-class LEDSerializer(serializers.ModelSerializer):
-    #user = serializers.ReadOnlyField(source='users.username')
+class LEDSerializer(serializers.HyperlinkedModelSerializer):
+    users = serializers.ReadOnlyField(source='users.username')
 
     class Meta:
         model = LED
-        fields = ['id','name','gpio_num','color','status','onoff','authenti_testing']
+        fields = ['id','name','gpio_num','color','status','onoff','authenti_testing','users']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    #LEDs = serializers.HyperlinkedRelatedField(many=True, view_name='LED-list', read_only=True)
-    LEDs = serializers.PrimaryKeyRelatedField(many=True, queryset=LED.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    LEDs = serializers.HyperlinkedRelatedField(many=True, view_name='LED-detail', read_only=True)
+    #LEDs = serializers.PrimaryKeyRelatedField(many=True, queryset=LED.objects.all())
+
     class Meta:
         model = User
         fields = ['id', 'username', 'LEDs']
