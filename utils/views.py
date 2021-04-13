16dafdf40d6143aa47e35f2e903dc3515453f2bb
Nyc <s2012439@yes.my>
2021-04-13 23:20:13 +0800

    delete unused stuffs

--- utils/views.py
+++ utils/views.py
@@ -1,19 +1,6 @@
 from rest_framework.decorators import api_view
 from utils.mail import sendmail
 from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-import os
-
-# @csrf_exempt
-# def certification(request):
-#     if request.method == "POST":
-#         print(os.getcwd())
-#         sendmail(request.POST["mail"])
-#         return JsonResponse({'success':True}, safe=False)
-#     return JsonResponse({'success':False}, safe=False)
-
-
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -21,12 +8,12 @@ from rest_framework.reverse import reverse
 @api_view(["GET", "POST"])
 def certification(request):
     if request.method == "POST":
-        print(os.getcwd())
         sendmail(request.POST["mail"])
         return Response({"success": True})
     return Response({"success": False})
 
 
+# endpoint
 @api_view(["GET"])
 def api_root(request, format=None):
     return Response(
