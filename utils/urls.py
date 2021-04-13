452186f724857b968ce5a83eee99084aeacb8c28
Nyc <s2012439@yes.my>
2021-04-13 22:58:59 +0800

    change user to client, add utils router)

--- utils/urls.py
+++ utils/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path,include
+from django.urls import path, include
 from utils import views
 
 
 urlpatterns = [
-    path('mail_certification/',views.certification)
+    path("mail_certification/", views.certification, name="certification"),
+    path("", views.api_root),
 ]
--- utils/views.py
+++ utils/views.py
@@ -4,10 +4,35 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import os
 
-@csrf_exempt
+# @csrf_exempt
+# def certification(request):
+#     if request.method == "POST":
+#         print(os.getcwd())
+#         sendmail(request.POST["mail"])
+#         return JsonResponse({'success':True}, safe=False)
+#     return JsonResponse({'success':False}, safe=False)
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(["GET", "POST"])
 def certification(request):
     if request.method == "POST":
         print(os.getcwd())
         sendmail(request.POST["mail"])
-        return JsonResponse({'success':True}, safe=False)
-    return JsonResponse({'success':False}, safe=False)
+        return Response({"success": True})
+    return Response({"success": False})
+
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "mail_certification": reverse(
+                "certification", request=request, format=format
+            ),
+        }
+    )
