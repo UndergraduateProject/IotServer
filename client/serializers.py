a9efeda2486e5d8f4f65b4aa848d43227d7732f0
Nyc <s2012439@yes.my>
2021-04-22 02:22:29 +0800

    add relationship

--- client/serializers.py
+++ client/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from client.models import Client
 
 
-class ClientSerializer(serializers.ModelSerializer):
+class ClientSerializer(serializers.HyperlinkedModelSerializer):
+
+    sensors = serializers.HyperlinkedIdentityField(
+        many=True,
+        read_only=True,
+        view_name="sensor-detail"
+    )
+
     class Meta:
         model = Client
-        fields = ["id", "username", "password", "email", "verify", "enable"]
+        fields = ["id", "username", "password",
+                  "email", "verify", "enable", "sensors"]
--- enviroment/models.py
+++ enviroment/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-#from ..client.models import Client
+from client.models import Client
 
 # Create your models here.
 
 
 class Sensor(models.Model):
-    #    clientID = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(
+        Client, related_name='sensors', on_delete=models.CASCADE)
     sensorID = models.IntegerField(primary_key=True, auto_created=True)
     sensorName = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.sensorName
 
 
 class Humid_Temp(models.Model):
@@ -15,10 +19,18 @@ class Humid_Temp(models.Model):
     humidity = models.FloatField()
     temperature = models.FloatField()
     heatIndex = models.FloatField(default=0)
-    # sensorID = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    sensor = models.ForeignKey(
+        Sensor, related_name='humid_temp', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'humidity : %d , temperature : %d' % (self.humidity, self.temperature)
 
 
 class Moisture(models.Model):
     created = models.DateTimeField()
     value = models.FloatField()
-    # sensorID = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    sensor = models.ForeignKey(
+        Sensor, related_name='moisture', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.value
