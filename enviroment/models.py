416b4abf2906e931985715e2fe508192b58ffc92
william50307 <william5030788@gmail.com>
2021-04-25 15:29:33 +0800

    modified models setting and chnage login page to api view

--- enviroment/models.py
+++ enviroment/models.py
@@ -5,31 +5,20 @@ from client.models import Client
 
 
 class Sensor(models.Model):
-<<<<<<< HEAD
-    clientID = models.ForeignKey(Client, related_name='sensors', on_delete=models.CASCADE, null=True)
-    sensorID = models.IntegerField(primary_key=True, auto_created=True)
-    sensorName = models.CharField(max_length=30)
-    
-=======
-    client = models.ForeignKey(
-        Client, related_name="sensors", on_delete=models.CASCADE, null=True
-    )
+    client = models.ForeignKey(Client, related_name="sensors", on_delete=models.CASCADE, null=True, blank=True)
     sensorID = models.IntegerField(primary_key=True, auto_created=True)
     sensorName = models.CharField(max_length=30)
 
     def __str__(self):
         return self.sensorName
 
->>>>>>> a95922caf693df3b893fe09f64e6753bfd77a21c
 
 class Humid_Temp(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     humidity = models.FloatField()
     temperature = models.FloatField()
     heatIndex = models.FloatField(default=0)
-    sensor = models.ForeignKey(
-        Sensor, related_name="humid_temp", on_delete=models.CASCADE, null=True
-    )
+    sensor = models.ForeignKey(Sensor, related_name="humid_temp", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return "humidity : %d , temperature : %d" % (self.humidity, self.temperature)
@@ -38,9 +27,7 @@ class Humid_Temp(models.Model):
 class Moisture(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     value = models.FloatField()
-    sensor = models.ForeignKey(
-        Sensor, related_name="moisture", on_delete=models.CASCADE, null=True
-    )
+    sensor = models.ForeignKey(Sensor, related_name="moisture", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return self.value
+        return str(self.value)
--- utils/views.py
+++ utils/views.py
@@ -4,7 +4,7 @@ from utils.socket_connection import SocketMsg
 from django.http import HttpResponse, JsonResponse
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-
+from django.contrib.auth import authenticate, login
 
 @api_view(["GET", "POST"])
 def certification(request):
@@ -22,11 +22,28 @@ def socket_connection(request):
         return Response({"success": True})
     return Response({"success": False})
 
+@api_view(["GET", "POST"])
+def login(request):
+    #print (request.user)
+    #return Response({"success" : False, "status" : "tesing"})
+    username = request.data.get("username", None)
+    password = request.data.get("password", None)
+    if not username:
+        return Response({"success" : False, "status" : "username is None"})
+    elif not password:
+        return Response({"success" : False, "status" : "password is None"})
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return Response({"success" : True , "status" : "logined"})
+    return Response({"success" : False, "status" : "Please check your username and password"})
+
 #utils endpoint
 @api_view(["GET"])
 def api_root(request, format=None):
     return Response({
         "mail_certification" : reverse("certification", request=request, format=format),
-        "socket_connection" : reverse("socket_connection", request=request, format=format)
-        }
-    )
+        "socket_connection" : reverse("socket_connection", request=request, format=format),
+        "login" : reverse("login", request=request, format=format),
+    })
