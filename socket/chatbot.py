0135391f88b0c7dc921f91c380d7422870ff13c5
Nyc <s2012439@yes.my>
2021-10-14 00:23:52 +0800

    chatbot kill print

--- socket/chatbot.py
+++ socket/chatbot.py
@@ -28,7 +28,7 @@ def get_response(text_to_be_analyzed="ni_chatbot"):
         response = session_client.detect_intent(session=session, query_input=query_input)
         #print(response)
     except InvalidArgument as e:
-        print(e)
+        # print(e)
         raise
     return response
 
@@ -44,7 +44,7 @@ def temperature():
     temperature = data['temperature']
     msg = response.query_result.fulfillment_text + str(temperature)
     sio.emit('chatbot', msg)
-    print(response.query_result.fulfillment_text, temperature)
+    # print(response.query_result.fulfillment_text, temperature)
 
 def humidity():
     res = rq.get(src)
@@ -56,36 +56,31 @@ def humidity():
     humidity = data['humidity']
     msg = response.query_result.fulfillment_text + str(humidity)
     sio.emit('chatbot', msg)
-    print(response.query_result.fulfillment_text, humidity)
+    # print(response.query_result.fulfillment_text, humidity)
 
 # action
 def action_watering():
-    print("action_watering ")
+    pass
 
 def action_light():
-    print("action_watering")
+    pass
     
 def action_fan():
-    print("action_fan")
-
-#information 
-def get_output(response):
-    print("input text:", response.query_result.query_text)
-    #print("intent:", response.query_result.intent.display_name)
-    #print("intent's confidence:", response.query_result.intent_detection_confidence)
-    print("response:", response.query_result.fulfillment_text)    
+    pass
+ 
 
 @sio.on('connect')
 def on_connect():
-    print('connection established')
+    # print('connection established')
+    pass
 
 @sio.on("chatbot")
 def on_message(data):
-    print("message" ,data)
+    # print("message" ,data)
     global response
     response = get_response(data)
     # get_output(response)
-    print("keyword: ", response.query_result.intent.display_name)
+    # print("keyword: ", response.query_result.intent.display_name)
     #command+shift+p -> interpreter-> copy bin/python
     keyword = response.query_result.intent.display_name
     confidence = response.query_result.intent_detection_confidence
@@ -102,7 +97,7 @@ def on_message(data):
         action_watering()
     elif keyword == "Default Fallback Intent":
         sio.emit("chatbot", "Sorry, what was that?")
-        print("Sorry, what was that?")
+        # print("Sorry, what was that?")
     else:
         sio.emit('chatbot', response.query_result.fulfillment_text)
 
@@ -110,7 +105,8 @@ def on_message(data):
 
 @sio.on('disconnect')
 def on_disconnect():
-    print('disconnected from server')
+    # print('disconnected from server')
+    pass
 
 sio.connect("http://140.117.71.98:4001")
 
